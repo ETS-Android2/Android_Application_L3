4ab2eebdc77477ebc74840528e561e7948dacc95
Sebastien <marrosebastien@protonmail.com>
2020-03-05 11:51:49 +0100

    Redirection pour activité Login + reformatage #21
    onClickListener qui renvoie vers activité TeachingUnitActivity

--- Android/app/src/main/java/fr/info/pl2020/activity/LoginActivity.java
+++ Android/app/src/main/java/fr/info/pl2020/activity/LoginActivity.java
@@ -2,7 +2,10 @@ package fr.info.pl2020.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import fr.info.pl2020.R;
 
@@ -12,5 +15,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Button button = (Button) findViewById(R.id.loginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, TeachingUnitActivity.class));
+            }
+        });
     }
 }
