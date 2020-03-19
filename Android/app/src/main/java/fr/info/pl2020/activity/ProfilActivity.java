a7f266452a3408490ffaa7a556ace6f4392000cc
yasmine <yasmine.ben-fredj@etu.unice.fr>
2020-03-19 17:30:02 +0100

    Bouton "site" renvois vers un site.

--- Android/app/src/main/java/fr/info/pl2020/activity/ProfilActivity.java
+++ Android/app/src/main/java/fr/info/pl2020/activity/ProfilActivity.java
@@ -1,9 +1,12 @@
 package fr.info.pl2020.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
 import fr.info.pl2020.R;
 
 public class ProfilActivity extends AppCompatActivity {
@@ -12,5 +15,16 @@ public class ProfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        Button button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 1. Appeler une URL web
+                String url = "http://univ-cotedazur.fr/";
+                Intent intent = new Intent( Intent.ACTION_VIEW, Uri.parse(url) );
+                startActivity(intent);
+            }
+        });
     }
+
 }
