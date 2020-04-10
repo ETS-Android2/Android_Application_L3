ceca4b31f0259d939f4e22f8ae03d36f12807e16
jasmine <yasmine.ben-fredj@etu.unice.fr>
2020-04-10 03:48:37 +0200

    Test

--- Android/app/src/main/java/fr/info/pl2020/activity/CareerSummaryActivity.java
+++ Android/app/src/main/java/fr/info/pl2020/activity/CareerSummaryActivity.java
@@ -2,47 +2,42 @@ package fr.info.pl2020.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import java.util.ArrayList;
+
 import fr.info.pl2020.R;
+import fr.info.pl2020.adapter.CareerSummaryAdapter;
 import fr.info.pl2020.controller.CareerController;
 import fr.info.pl2020.controller.TeachingUnitController;
+import fr.info.pl2020.model.TeachingUnitListContent;
+import fr.info.pl2020.service.CareerService;
 
 public class CareerSummaryActivity extends AppCompatActivity {
     private ListView listTU ;
-    private ListView semesters ;
     private CareerController careerController;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_career_summary);
-        this.semesters  = findViewById(R.id.Semester_career_summary);
+        listTU = findViewById(R.id.teBySemester);
 
         // Récupération de la liste des UE
         this.careerController = new CareerController();
-        this.careerController.getCareer(this ,() -> {});
-
-
-
-
-
-
-
-
-
-
-
-
-
+        this.careerController.getCareer(this,() -> {
+            CareerSummaryAdapter adapter = new CareerSummaryAdapter(CareerSummaryActivity.this, new ArrayList<>( TeachingUnitListContent.TEACHING_UNITS.values()));
+            listTU.setAdapter(adapter);
+        });
 
 
     }
-
-
-
-
 
 
 
---
+++ Android/app/src/main/java/fr/info/pl2020/adapter/CareerSummaryAdapter.java
@@ -0,0 +1,53 @@
+package fr.info.pl2020.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import fr.info.pl2020.R;
+
+import fr.info.pl2020.model.TeachingUnitListContent;
+
+public class CareerSummaryAdapter extends BaseAdapter {
+    private Context context;
+    private List<TeachingUnitListContent.TeachingUnit>  teachingUnits ;
+
+    public CareerSummaryAdapter(Context context, List<TeachingUnitListContent.TeachingUnit> teachingUnits){
+        this.context=context;
+        this.teachingUnits=teachingUnits;
+    }
+
+    @Override
+    public int getCount() {
+        return teachingUnits.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.teachingUnits.get(position) ;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_career_summary, null);
+        }
+
+        TeachingUnitListContent.TeachingUnit teachingUnit = (TeachingUnitListContent.TeachingUnit) getItem(position);
+        TextView teachingUnitTextView = convertView.findViewById(R.id.career);
+        teachingUnitTextView.setText(teachingUnit.getName());
+        return convertView;
+    }
+}
